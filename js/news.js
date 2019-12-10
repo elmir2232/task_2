33f0e4fdac772adee088e79e15dd0c149f1d706b
elmir2232 <57491669+elmir2232@users.noreply.github.com>
2019-12-10 17:11:20 +0200

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,3 +1,16 @@
+window.onload = function(){
+
+	let adminKey = localStorage.getItem("isAdmin");
+	if (adminKey === "false"){
+	document.getElementById('button_addNews').style.visibility = "hidden";
+	}
+	else {
+		
+		console.log("admin")
+	}
+}
+
+
 document.getElementById('button_logout').onclick = function(){
 	document.location.assign('index.html');
 	localStorage.removeItem("isAdmin");}
